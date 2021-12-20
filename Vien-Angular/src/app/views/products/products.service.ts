454ae0e62444a04824a2317df80adf8658a92d3e
Sarvar Murad <11269472+sarvarkhuja@users.noreply.github.com>
2021-12-20 19:38:30 +0500

    Update products.service.ts

--- Vien-Angular/src/app/views/products/products.service.ts
+++ Vien-Angular/src/app/views/products/products.service.ts
@@ -3,21 +3,15 @@ import { Injectable } from "@angular/core";
 import { User } from "firebase";
 import { Observable } from "rxjs";
 import { ServiceHelper } from "src/app/core/helpers/service.helper";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { get } from "http";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductsService {
-  constructor(private httpc:HttpClient) {}
+  constructor() {}
 
   userProfilesGet(): Observable<User> {
-    let clientheaders = new HttpHeaders({
-      'Content-Type': 'multipart/form-data',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    });
-    return this.httpc.get<User>(UrlSettings.BASE_URL + `Web/GetProducts`, {headers:clientheaders});
+    return ServiceHelper.get<User>(UrlSettings.BASE_URL + `Web/GetProducts`);
   }
 }
+  
