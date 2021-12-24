c182d34c2c602319cbb2d30d3eb32d6527e39e4c
Sarvar Murad <11269472+sarvarkhuja@users.noreply.github.com>
2021-12-24 06:59:20 +0500

    Revert "Update products.service.ts"
    This reverts commit 454ae0e62444a04824a2317df80adf8658a92d3e.

--- Vien-Angular/src/app/views/products/products.service.ts
+++ Vien-Angular/src/app/views/products/products.service.ts
@@ -3,15 +3,21 @@ import { Injectable } from "@angular/core";
 import { User } from "firebase";
 import { Observable } from "rxjs";
 import { ServiceHelper } from "src/app/core/helpers/service.helper";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { get } from "http";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductsService {
-  constructor() {}
+  constructor(private httpc:HttpClient) {}
 
   userProfilesGet(): Observable<User> {
-    return ServiceHelper.get<User>(UrlSettings.BASE_URL + `Web/GetProducts`);
+    let clientheaders = new HttpHeaders({
+      'Content-Type': 'multipart/form-data',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+    return this.httpc.get<User>(UrlSettings.BASE_URL + `Web/GetProducts`, {headers:clientheaders});
   }
 }
-  
